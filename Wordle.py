f902074efd988168ba4ba2d5407eb6026abca9e1
Juniperammirati <jbammirati@willamette.edu>
2024-09-25 10:50:36 -0700

    Update Wordle.py

--- Wordle.py
+++ Wordle.py
@@ -1,8 +1,8 @@
 ########################################
-# Name:
-# Collaborators (if any):
-# GenAI Transcript (if any):
-# Estimated time spent (hr):
+# Name: Juniper Ammirati
+# Collaborators (if any): Kendall
+# GenAI Transcript (if any): 
+# Estimated time spent (hr): 1(day one)
 # Description of any added extensions:
 ########################################
 
@@ -12,10 +12,22 @@ import random
 
 def wordle():
     # The main function to play the Wordle game.
-
+    def my_guess():
+        my_letters = []
+        for i in range (5):
+            my_letters.append (gw.get_square_letter(0,i))
+        print(my_letters)
+        my_guess = "".join(my_letters)
+        print(my_guess)
+        return my_guess
+    
     def enter_action():
         # What should happen when RETURN/ENTER is pressed.
-        gw.show_message("You need to implement this method")
+         if is_english_word (my_guess()):
+            gw.show_message ("true")
+        else: 
+            gw.show_message("not english")
+
 
 
     gw = WordleGWindow()
